f65a7b8b2d6c279f9ad463a8e9154c2fda47dcf2
cgrigsby <cgrigsby@celerik.com>
2022-01-26 22:20:48 -0500

    feat(writeFile): write cmd-line arg to file

--- write-file/make-list.c
+++ write-file/make-list.c
@@ -9,13 +9,20 @@ int main(int argc, char *argv[])
     }
     else
     {
+        char *user_num;
         char *file_name;
+        char str[5];
         FILE *fptr;
 
+        user_num = argv[1];
         file_name = argv[2];
-        fptr = fopen(file_name, "w");
-        fprintf(fptr, "Hello World");
 
+        fptr = fopen(file_name, "w");
+        for (int i = 0; i < atoi(user_num); ++i)
+        {
+            sprintf(str, "%d", i);
+            fprintf(fptr, "%s\n", str);
+        }
         fclose(fptr);
     }
     return 0;
